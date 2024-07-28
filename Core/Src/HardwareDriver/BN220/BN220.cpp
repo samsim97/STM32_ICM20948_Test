cae00aa51a322d3cffa1a4754b2f057c21f3e197
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-07-28 10:55:27 -0400

    Newlines

--- Core/Src/HardwareDriver/BN220/BN220.cpp
+++ Core/Src/HardwareDriver/BN220/BN220.cpp
@@ -8,6 +8,8 @@ BN220::BN220(UART_HandleTypeDef* uartHandle)
 void BN220::readGPS()
 {
 	int returnCode = HAL_UART_Receive(uartHandle, buffer, sizeof(buffer) - 1, 5000);
+
+
 }
 
 GPSValues BN220::getGPSValues()
